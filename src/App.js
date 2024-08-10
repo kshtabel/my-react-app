0f27832e95205e18d81c1c5cc65538ead369c3c8
kshtabel <konstantin.shtabel@outlook.com>
2024-08-10 21:53:39 +0200

    Renewed App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+
+//         <p>
+//           Editet some text, and reloaded...
+//         </p>
+
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [data, setData] = useState('');
+
+  useEffect( () => {
+
+    axios.get('/api/data')
+    .then(response => {
+      setData(response.data.message);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }, []);
 }
 
-export default App;
+return (
+  <div className='App'>
+    <h1>React and Node.js Integration</h1>
+    <p>Message from the server: {data}</p>
+  </div>
+);
