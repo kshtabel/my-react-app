4008e43e9c1745dd14dc38f6f70d74a6fdb7203c
kshtabel <konstantin.shtabel@outlook.com>
2024-08-12 00:15:30 +0200

    Delete App import

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import './index.css';
 import Navigation from './main';
-import App from './App';
-// import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navigation />
-    <App />
   </React.StrictMode>
 );
