75dbcf7fd9871cbdbbb9f0f36b5cee383d19588f
kshtabel <konstantin.shtabel@outlook.com>
2024-08-12 00:50:12 +0200

    Added event OnButtonClick for routing purposes - Not working !

--- src/login_View.js
+++ src/login_View.js
@@ -1,6 +1,38 @@
-const OnButtonClick = (event) => {
-    event.preventDefault();
-    alert('Button clicked');
+import * as React from 'react';
+import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+
+// export default function View_Login() {
+//     return (
+//         <div>
+//             <Stack
+//                 direction='column'
+//                 divider={<Divider orientation='horizontal' flexItem />}
+//                 spacing={2}
+//                 >
+
+//                 <TextField id='filled-basic' label='Benutzername' variant='filled' />
+
+//             </Stack>
+//         </div>
+//     )
+// }
+
+const OnButtonClick = () => {
+    return (
+        <div>
+            <Stack
+                direction='column'
+                divider={<Divider orientation='horizontal' flexItem />}
+                spacing={2}
+                >
+
+                <TextField id='filled-basic' label='Benutzername' variant='filled' />
+
+            </Stack>
+        </div>
+    )
 }
 
 export default OnButtonClick;
