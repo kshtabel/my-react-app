d5672806d152fcf9c7d7bd75d947a0fb7088b2a2
kshtabel <konstantin.shtabel@outlook.com>
2024-08-23 23:53:51 +0200

    Added authentication

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,40 +1,37 @@
-// LoginPage.js
 import * as React from 'react';
 import Divider from '@mui/material/Divider';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import Box from '@mui/material/Box';
-import {useState} from 'react';
+import { useState } from 'react';
 import axios from 'axios';
-
+import UserBoard from './User_Board';
+import { useAuth } from '../../context/AuthContext'; // Importieren Sie den AuthContext
 
 const Login = () => {
-
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [emailError, setEmailError] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
+    const { isLoggedIn, login } = useAuth(); // Verwenden Sie den AuthContext
 
     const handleSubmit = async (e) => {
-
         e.preventDefault();
-        
 
         setEmailError(false);
         setPasswordError(false);
 
-        if (email == '') {
+        if (email === '') {
             setEmailError(true);
         }
 
-        if (password == '') {
+        if (password === '') {
             setPasswordError(true);
         }
 
         if (email && password) {
             try {
-                //send post request to server
                 const response = await axios.post('api/login', {
                     email: email,
                     password: password
@@ -42,11 +39,17 @@ const Login = () => {
 
                 console.log('Server response: ', response.data);
                 alert('Login erfolgreich');
-            } catch(error) {
+
+                login(response.data.token); // Verwenden Sie die login-Funktion aus dem AuthContext
+            } catch (error) {
                 console.error('Fehler bei der Verarbeitung ', error);
                 alert('Fehler bei der Verarbeitung');
             }
         }
+    }
+
+    if (isLoggedIn) {
+        return <UserBoard />;
     }
 
     return (
@@ -55,7 +58,7 @@ const Login = () => {
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                height: '50vh', // Höhe des gesamten Viewports
+                height: '50vh',
             }}
         >
             <form onSubmit={handleSubmit}>
@@ -64,7 +67,7 @@ const Login = () => {
                     divider={<Divider orientation='horizontal' flexItem />}
                     spacing={2}
                     sx={{
-                        width: '300px', // Breite des Formulars
+                        width: '300px',
                     }}
                 >
                     <TextField 
