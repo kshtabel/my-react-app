6bf932edae08fa2fd1bea914d67a537e5862aebf
kshtabel <konstantin.shtabel@outlook.com>
2024-08-27 21:36:30 +0200

    Added dynamically presentation of Appbar after user logged in

--- src/header.js
+++ src/header.js
@@ -1,4 +1,3 @@
-// src/header.js
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -7,43 +6,76 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
-import { useAuth } from './context/AuthContext'; // Importieren Sie den AuthContext
+import { useAuth } from './context/AuthContext'; // Import AuthContext
 
 export default function Navigation() {
-  const { isLoggedIn, logout } = useAuth(); // Verwenden Sie den AuthContext
+  const { isLoggedIn, logout } = useAuth(); // Zugriff auf Auth-Status und Logout-Funktion
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
+      <AppBar position="static">
         <Toolbar>
           <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='menu'
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
             sx={{ mr: 2 }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Startseite
           </Typography>
-          <Button component={Link} to='/' color='inherit'>Startseite</Button>
+
+          {/* Dynamische Anzeige basierend auf Login-Status */}
           {!isLoggedIn ? (
             <>
-              <Button component={Link} to='/login' color='inherit'>
-                Login
-              </Button>
-              <Button component={Link} to='/register' color='inherit'>
-                Registrieren
-              </Button>
+              <Button component={Link} to="/" color="inherit">Startseite</Button>
+              <Button component={Link} to="/login" color="inherit">Login</Button>
+              <Button component={Link} to="/register" color="inherit">Registrieren</Button>
             </>
           ) : (
             <>
-              <Button onClick={logout} color='inherit'>
-                Logout
-              </Button>
+              <Button color="inherit" onClick={logout}>Logout</Button>
+              <IconButton
+                size="large"
+                edge="end"
+                color="inherit"
+                onClick={handleMenu}
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Account</MenuItem>
+                <MenuItem onClick={handleClose}>Einstellungen</MenuItem>
+              </Menu>
             </>
           )}
         </Toolbar>
